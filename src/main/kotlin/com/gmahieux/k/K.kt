c1b4e592c4b6a04efca3da467a619bc842eb0e2b
Pierre Leresteux <pierre.leresteux@gmail.com>
2019-03-25 14:16:59 +0100

    Add mongo

--- src/main/kotlin/com/gmahieux/k/K.kt
+++ src/main/kotlin/com/gmahieux/k/K.kt
@@ -3,6 +3,9 @@ package com.gmahieux.k
 import com.gmahieux.k.data.Quotes
 import com.gmahieux.k.model.Quote
 import com.google.gson.Gson
+import com.mongodb.MongoCredential
+import com.mongodb.ServerAddress
+import com.mongodb.client.MongoCollection
 import io.ktor.application.call
 import io.ktor.http.ContentType
 import io.ktor.response.respondText
@@ -10,9 +13,28 @@ import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import org.litote.kmongo.KMongo
+import org.litote.kmongo.MongoOperator.sample
+import org.litote.kmongo.aggregate
+import org.litote.kmongo.getCollection
+import java.util.Arrays.asList
 
 fun main() {
     val quotes = Quotes()
+
+    val client = KMongo.createClient(
+        ServerAddress("localhost", 27017),
+        asList(
+            MongoCredential.createCredential(
+                "mongoadmin",
+                "admin",
+                "secret".toCharArray()
+            )
+        )
+    )
+    val database = client.getDatabase("k")
+    val col = database.getCollection<Quote>()
+    insertQuoteIfMissing(col, quotes)
 
     val server = embeddedServer(Netty, port = 8080) {
         routing {
@@ -21,13 +43,23 @@ fun main() {
                 call.respondText("Hello World!", ContentType.Text.Plain)
             }
             get("/quote") {
+                val quote = col.aggregate<Quote>("""[{ ${sample}: { size: 1 } }]""").first()
                 call.respondText(
                     contentType = ContentType.Application.Json,
-                    text = Gson().toJson(quotes.getRandomQuote(), Quote::class.java)
+                    text = Gson().toJson(quote, Quote::class.java)
                 )
             }
         }
     }
     server.start(wait = true)
+}
+
+private fun insertQuoteIfMissing(
+    col: MongoCollection<Quote>,
+    quotes: Quotes
+) {
+    if (col.countDocuments() == 0L) {
+        col.insertMany(quotes.quotes)
+    }
 }
 
